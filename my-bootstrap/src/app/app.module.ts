3f48d03daeb324fa2913aaeb0cbb3e33877154dc
Lindan Thillanayagam <lindx4@gmail.com>
2018-11-18 22:35:20 -0500

    Nested servers

--- my-bootstrap/src/app/app.module.ts
+++ my-bootstrap/src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-//Testing
-
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule
