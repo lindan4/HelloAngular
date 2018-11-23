0ca9bfd4f2c2a37ecfa9f5ae9cc0692a9526a03b
Lindan Thillanayagam <lindx4@gmail.com>
2018-11-22 22:13:40 -0500

    Practiced string interpolation alongside property binding

--- my-bootstrap/src/app/servers/servers.component.ts
+++ my-bootstrap/src/app/servers/servers.component.ts
@@ -3,16 +3,41 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   //selector: '[app-servers]',
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+
+  serverCreationStatus  = 'No server has been created. To add a new server, input its name above and press \"Add Server\"';
+  serverName = '';
+
+
+  constructor() {
+
+    setTimeout(() => {
+      this.allowNewServer = true;
+
+    }, 3000);
+  }
 
   ngOnInit() {
   }
 
+
+  onCreateServer() {
+    if (this.serverCreationStatus.length === 0) {
+      this.serverCreationStatus = 'The server does not have a name. Please try again.';
+    } else {
+      this.serverCreationStatus = 'The server' + this.serverName + 'was created';
+    }
+
+  }
+
+  onUpdateServerName(event: Event) {
+
+    this.serverName = ((<HTMLInputElement>event.target).value.length === 0) ? '' : 'New server name: ' + (<HTMLInputElement>event.target).value;
+
+  }
 }
